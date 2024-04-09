4537c016f603217f29334f414c27ade8c145299e
plaiaundi <plaiaundi@MININT-5OLIS4G>
2024-04-09 20:14:51 +0200

    commit

--- interfazgrafica/src/interfazgrafica/Interfaz.java
+++ interfazgrafica/src/interfazgrafica/Interfaz.java
@@ -3,130 +3,90 @@ package interfazgrafica;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
-import javax.swing.JLabel;
+import javax.swing.JPanel;
 import java.awt.BorderLayout;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
 import javax.swing.JTextField;
-import java.awt.Label;
-import java.awt.GridLayout;
-import javax.swing.BoxLayout;
-import com.jgoodies.forms.layout.FormLayout;
-import com.jgoodies.forms.layout.ColumnSpec;
-import com.jgoodies.forms.layout.RowSpec;
-import com.jgoodies.forms.layout.FormSpecs;
-import javax.swing.JCheckBox;
 import javax.swing.JButton;
-import javax.swing.JTextPane;
+import javax.swing.JCheckBox;
+import java.awt.Color;
+import javax.swing.UIManager;
 
 public class Interfaz {
 
-	private JFrame frame;
-	private JTextField textUsuario;
-	private JTextField textContraseña;
+    private JFrame frame;
+    private JTextField txtRgeger;
+    private JTextField textField;
+    private String usuario;
+    private String contraseña;
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					Interfaz window = new Interfaz();
-					window.frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+    /**
+     * Launch the application.
+     */
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    Interfaz window = new Interfaz();
+                    window.frame.setVisible(true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
 
-	/**
-	 * Create the application.
-	 */
-	public Interfaz() {
-		initialize();
-	}
+    /**
+     * Create the application.
+     */
+    public Interfaz() {
+        initialize();
+    }
 
-	/**
-	 * Initialize the contents of the frame.
-	 */
-	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(new FormLayout(new ColumnSpec[] {
-				FormSpecs.RELATED_GAP_COLSPEC,
-				FormSpecs.DEFAULT_COLSPEC,
-				FormSpecs.RELATED_GAP_COLSPEC,
-				FormSpecs.DEFAULT_COLSPEC,
-				FormSpecs.RELATED_GAP_COLSPEC,
-				FormSpecs.DEFAULT_COLSPEC,
-				FormSpecs.RELATED_GAP_COLSPEC,
-				FormSpecs.DEFAULT_COLSPEC,
-				FormSpecs.RELATED_GAP_COLSPEC,
-				FormSpecs.DEFAULT_COLSPEC,
-				FormSpecs.RELATED_GAP_COLSPEC,
-				FormSpecs.DEFAULT_COLSPEC,
-				FormSpecs.RELATED_GAP_COLSPEC,
-				FormSpecs.DEFAULT_COLSPEC,
-				FormSpecs.RELATED_GAP_COLSPEC,
-				ColumnSpec.decode("default:grow"),
-				FormSpecs.RELATED_GAP_COLSPEC,
-				ColumnSpec.decode("default:grow"),},
-			new RowSpec[] {
-				FormSpecs.RELATED_GAP_ROWSPEC,
-				FormSpecs.DEFAULT_ROWSPEC,
-				FormSpecs.RELATED_GAP_ROWSPEC,
-				FormSpecs.DEFAULT_ROWSPEC,
-				FormSpecs.RELATED_GAP_ROWSPEC,
-				FormSpecs.DEFAULT_ROWSPEC,
-				FormSpecs.RELATED_GAP_ROWSPEC,
-				FormSpecs.DEFAULT_ROWSPEC,
-				FormSpecs.RELATED_GAP_ROWSPEC,
-				FormSpecs.DEFAULT_ROWSPEC,
-				FormSpecs.RELATED_GAP_ROWSPEC,
-				FormSpecs.DEFAULT_ROWSPEC,
-				FormSpecs.RELATED_GAP_ROWSPEC,
-				FormSpecs.DEFAULT_ROWSPEC,
-				FormSpecs.RELATED_GAP_ROWSPEC,
-				FormSpecs.DEFAULT_ROWSPEC,
-				FormSpecs.RELATED_GAP_ROWSPEC,
-				FormSpecs.DEFAULT_ROWSPEC,
-				FormSpecs.RELATED_GAP_ROWSPEC,
-				RowSpec.decode("default:grow"),}));
-		
-		JLabel usuario = new JLabel("usuario");
-		frame.getContentPane().add(usuario, "12, 4, left, default");
-		
-		textUsuario = new JTextField();
-		frame.getContentPane().add(textUsuario, "12, 6, fill, default");
-		textUsuario.setColumns(10);
-		
-		JLabel contraseña = new JLabel("contraseña");
-		frame.getContentPane().add(contraseña, "12, 8, left, default");
-		
-		textContraseña = new JTextField();
-		frame.getContentPane().add(textContraseña, "12, 10, fill, default");
-		textContraseña.setColumns(10);
-		
-		JCheckBox recordar = new JCheckBox("recordar este usuario");
-		frame.getContentPane().add(recordar, "12, 14, center, default");
-		
-		JButton Login = new JButton("Login");
-		frame.getContentPane().add(Login, "12, 16, center, default");
-		
-		JTextPane textPane = new JTextPane();
-		frame.getContentPane().add(textPane, "12, 20, default, fill");
-		
-		Login.addActionListener(new java.awt.event.ActionListener() {
-			public void actionPerformed(java.awt.event.ActionEvent e)
-			{
-				
-				String nombreUsuario = textUsuario.getText();
-				System.out.println(nombreUsuario);
-				
-				}
-		});
+    /**
+     * Initialize the contents of the frame.
+     */
+    private void initialize() {
+        frame = new JFrame();
+        frame.setBounds(100, 100, 450, 300);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().setLayout(null);
 
-	}
+        JLabel lblNewLabel = new JLabel("Usuario:");
+        lblNewLabel.setBounds(165, 31, 46, 14);
+        frame.getContentPane().add(lblNewLabel);
 
+        txtRgeger = new JTextField();
+        txtRgeger.setBounds(165, 48, 86, 20);
+        frame.getContentPane().add(txtRgeger);
+        txtRgeger.setColumns(10);
+
+        JLabel lblNewLabel_1 = new JLabel("Contraseña");
+        lblNewLabel_1.setBounds(165, 96, 86, 14);
+        frame.getContentPane().add(lblNewLabel_1);
+
+        textField = new JTextField();
+        textField.setBounds(165, 121, 86, 20);
+        frame.getContentPane().add(textField);
+        textField.setColumns(10);
+
+        JCheckBox chckbxNewCheckBox = new JCheckBox("Recordar esta usuario");
+        chckbxNewCheckBox.setBounds(154, 148, 131, 23);
+        frame.getContentPane().add(chckbxNewCheckBox);
+
+        JButton btnNewButton = new JButton("LOGIN");
+        btnNewButton.setForeground(UIManager.getColor("Button.darkShadow"));
+        btnNewButton.setBounds(165, 191, 89, 23);
+        frame.getContentPane().add(btnNewButton);
+
+        btnNewButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent e) {
+                usuario = txtRgeger.getText();
+                System.out.println("usuario: " + usuario);
+                contraseña = textField.getText();
+                System.out.println("contraseña: " + contraseña);
+            }
+        });
+    }
 }
